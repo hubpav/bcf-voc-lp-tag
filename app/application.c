744020f289105e9cdcfca3de530544305057c2f5
Pavel Hübner <pavel.hubner@gmail.com>
2018-11-08 23:55:40 +0100

    Update

--- app/application.c
+++ app/application.c
@@ -1,20 +1,29 @@
 #include <application.h>
 
-// LED instance
 bc_led_t led;
 
-// Button instance
-bc_button_t button;
+bc_sgpc3_t sgpc3;
 
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+void sgpc3_event_handler(bc_sgpc3_t *self, bc_sgpc3_event_t event, void *event_param)
 {
-    if (event == BC_BUTTON_EVENT_PRESS)
+    if (event == BC_SGPC3_EVENT_ERROR)
     {
-        bc_led_set_mode(&led, BC_LED_MODE_TOGGLE);
+        bc_log_error("SPGC3: Event error");
     }
+    else if (event == BC_SGPC3_EVENT_UPDATE)
+    {
+        uint16_t value;
 
-    // Logging in action
-    bc_log_info("Button event handler - event: %i", event);
+        if (bc_sgpc3_get_tvoc_ppb(self, &value))
+        {
+            bc_log_info("SPGC3: Event update (%u ppb)", value);
+
+            if (value != 0)
+            {
+                bc_led_pulse(&led, 100);
+            }
+        }
+    }
 }
 
 void application_init(void)
@@ -24,18 +33,9 @@ void application_init(void)
 
     // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
-    bc_led_set_mode(&led, BC_LED_MODE_ON);
+    bc_led_pulse(&led, 1000);
 
-    // Initialize button
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
-}
-
-void application_task(void)
-{
-    // Logging in action
-    bc_log_debug("application_task run");
-
-    // Plan next run this function after 1000 ms
-    bc_scheduler_plan_current_from_now(1000);
+    bc_sgpc3_init(&sgpc3, BC_I2C_I2C0, 0x58);
+    bc_sgpc3_set_event_handler(&sgpc3, sgpc3_event_handler, NULL);
+    bc_sgpc3_set_update_interval(&sgpc3, 5000);
 }
